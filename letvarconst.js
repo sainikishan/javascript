1849c56073c57bf538634792484da2e1514fcfe2
Kishan Saini <YOUR_GITHUB_EMAIL>
2026-08-13 10:29:55 +0530

    Update JavaScript code

---
+++ letvarconst.js
@@ -0,0 +1,16 @@
+// const ek bhar declare akr diyachnage nhi ho ga//
+const accountId = 132 //change nhi ho value
+let accountemail="kishansainni@gmail.com"//
+var accountpass="123456"//dont use
+accountcity="jaipur"
+let accountstate;
+//accountId=2//not allowed
+accountemail="hc@hc.com"
+accountpass="212121"
+accountcity="kanpur"
+console.log(accountId);
+/*
+prefer not to ue var
+bcz of issuse in block scope and functional scope
+*/
+console.table([accountId,accountemail,accountpass,accountcity,accountstate])
