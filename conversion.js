5e9b4abdc1900e3a3a8764bc53dc5e9f7df554f4
Kishan Saini <kishansaini666@gmail.com>
2026-08-17 17:24:13 +0530

    Update JavaScript code

--- conversion.js
+++ conversion.js
@@ -1,4 +1,4 @@
-let score = undefined
+let score = true
 // const {score} = req.body
 console.log(typeof score);
 console.log(typeof (score));
@@ -7,3 +7,11 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
+//"33"=>33
+//"33abc" =>NaN
+//"true"=>1
+//flase=> 0
+let isloggedIn= "kishan"
+let booleanilogeedIn=Boolean(isloggedIn)
+console.log(booleanilogeedIn)
+console.log(typeof booleanilogeedIn)
