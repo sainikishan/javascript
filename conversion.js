e431f35cfddb499b18a4d66f655281a46ec5a54c
Kishan Saini <kishansaini666@gmail.com>
2026-08-17 17:20:33 +0530

    Update JavaScript code

--- conversion.js
+++ conversion.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+let score = undefined
 // const {score} = req.body
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,3 +6,4 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
