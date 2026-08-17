ebb714a1e4f5d0abb8b5ec098af2d433153c6ed7
Kishan Saini <kishansaini666@gmail.com>
2026-08-17 17:09:02 +0530

    Update JavaScript code

--- datatypes.js
+++ datatypes.js
@@ -2,3 +2,14 @@
 //alert("hello") we are using nodejs,not browser
 console.log(3+3); //code readabolity should be high
 console.log("kisham")
+let name="hitesh"
+let age=18
+let isloggedIn=false
+//number =>  2 to power 53 /52
+//bigint
+//string => " "
+//boolean=>true/false
+// null=> standalone value
+//undefined => empty value represent
+
+console.log(typeof "hitesh");
