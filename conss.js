c811970d20db53aacf33185b0594f10097690450
Kishan Saini <kishansaini666@gmail.com>
2026-08-18 16:17:17 +0530

    Update JavaScript code

---
+++ conss.js
@@ -0,0 +1,8 @@
+let score = "33abc"
+
+// const {score}=req.body
+console.log(typeof score);
+console.log(typeof (score))
+let valueInNumber = String (score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
