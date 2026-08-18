b7635d4ae65cad26f19b3922abd702e262c9f08f
Kishan Saini <kishansaini666@gmail.com>
2026-08-18 16:25:48 +0530

    Update JavaScript code

--- conss.js
+++ conss.js
@@ -6,3 +6,18 @@ console.log(typeof (score))
 let valueInNumber = String (score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber);
+//"33" =>33
+//"33abc"=>NaN
+//true=?1
+//false=>0
+let isloggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let someNumber=33
+let stringnumber=String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
