4bde6aca89bc7a5322284810fa2646fb3ced075a
Kishan Saini <kishansaini666@gmail.com>
2026-08-18 16:40:45 +0530

    Update JavaScript code

--- operations.js
+++ operations.js
@@ -11,3 +11,13 @@ console.log(str3)
 console.log("1"+3+2)
 console.log(1+3+"2")
 console.log((3+4) * 5 % 3)
+console.log(+true)
+console.log(+"")
+
+ let num1,num2,num3
+ num1 = num2 = num3 = 2+2
+
+ let gamecounter=100
+ gamecounter++;
+ console.log(gamecounter);
+
