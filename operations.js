dcd9fb6a3b973947252379bd99103bd7a81d1401
Kishan Saini <kishansaini666@gmail.com>
2026-08-18 16:35:21 +0530

    Update JavaScript code

---
+++ operations.js
@@ -0,0 +1,13 @@
+let value =3
+let negvalue=-value
+console.log(negvalue)
+//operations
+console.log(2+2)
+//tricklet 
+let str1="hello"
+let str2 = "kishan"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1"+3+2)
+console.log(1+3+"2")
+console.log((3+4) * 5 % 3)
