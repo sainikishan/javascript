6a335ced3e80bcd2b719310d1fc1e55c45e206d9
Kishan Saini <kishansaini666@gmail.com>
2026-08-19 12:03:13 +0530

    Update JavaScript code

--- number.js
+++ number.js
@@ -11,3 +11,13 @@ console.log(balance.toFixed(2));
 const othernumber=123.5646
 console.log(othernumber.toPrecision(3));
 
+const hund=100000000
+console.log(hund.toLocaleString('en-IN'))
+
+//max and min
+//math--------------------------------------------------------------------------------
+
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.round(2.643434));
+console.log(Math.ceil(5.9));
