e55b233160792983e6c30772fc6cf309c697b8f0
Kishan Saini <kishansaini666@gmail.com>
2026-08-19 11:34:50 +0530

    Update JavaScript code

---
+++ string.js
@@ -0,0 +1,14 @@
+const name="kishan"
+const repo =10
+console.log(name + repo + "saini") //this is outdated
+console.log(`hello my name ${name} and repo ${repo}`)
+
+
+const gamename =new String ('ksihan')
+console.log(gamename[1])
+console.log(gamename._proto_)
+
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.toLowerCase())
+console.log(gamename.charAt(3))
