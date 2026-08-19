f7bc8dcf678bee8fdbce9b9ed24ee5dc181278b8
Kishan Saini <kishansaini666@gmail.com>
2026-08-19 11:55:40 +0530

    Update JavaScript code

---
+++ number.js
@@ -0,0 +1,13 @@
+const score =  400
+// console.log(typeof score);
+
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const othernumber=123.5646
+console.log(othernumber.toPrecision(3));
+
