2220f78d25e517e106dc072ae80b8416ffcef760
Kishan Saini <kishansaini666@gmail.com>
2026-08-19 11:23:15 +0530

    Update JavaScript code

--- stack.js
+++ stack.js
@@ -1,4 +1,16 @@
 //stack(primitive),heap memory(non prmitive)
 let myyooutube="kishansaini"
 let anothername= myyooutube
+anothername="kumar"
 console.log(anothername)
+
+let user={
+    email: "user@gmail.com",
+    upi: "user#upi.ocm"
+
+}
+let usertwo=user
+usertwo.email= "user@gmail.coms"
+
+console.log(user.email)
+console.log(usertwo.email)
