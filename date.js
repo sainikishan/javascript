6946e95c245659287147b5e7ef9f2a6bebc952f0
Kishan Saini <kishansaini666@gmail.com>
2026-08-20 16:16:33 +0530

    Update project

--- date.js
+++ date.js
@@ -3,5 +3,9 @@ console.log(myDate.toString());
 console.log(myDate.toLocaleDateString());
 console.log(typeof myDate);
 
-let mycreated= new Date(2026,0,23)
-console.log(mycreated.toDateString());
+let mycreated= new Date(2026,0,23,5,3)
+console.log(mycreated.toLocaleString());
+
+let date= new Date("2023-01-14")
+console.log(date.toLocaleString());
+//timestamp
